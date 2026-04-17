51e5cda05be07017534d575c3313571f516d5347
winsznx <timjosh507@gmail.com>
2026-04-17 21:33:23 +0100

    chore(constants): add barrel export for constants

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export { MAINNET_CHAIN_ID, TESTNET_CHAIN_ID, EXPLORER_URLS, explorerTxUrl, explorerAddressUrl } from './networks';
+export { CONTRACT_NAMES, STX_DECIMALS, MICRO_STX_PER_STX, MAX_MEMO_LENGTH } from './contracts';
+export { queryKeys } from './query-keys';
+
+export type {};
