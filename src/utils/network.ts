545281bb7829dd7ae8d773e59820a19d1682c83e
winsznx <timjosh507@gmail.com>
2026-04-17 21:33:45 +0100

    feat(utils): add network detection helpers

---
+++ src/utils/network.ts
@@ -0,0 +1,11 @@
+import { TESTNET_CHAIN_ID } from '../constants/networks';
+
+export function isTestnetChainId(chainId: number): boolean {
+  return chainId === TESTNET_CHAIN_ID;
+}
+
+export function networkLabel(isMainnet: boolean): string {
+  return isMainnet ? 'Mainnet' : 'Testnet';
+}
+
+export type {};
