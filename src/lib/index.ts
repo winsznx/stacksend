578754bb079cddb8ec843f63e33c37eae7d8e833
winsznx <timjosh507@gmail.com>
2026-04-17 21:31:22 +0100

    chore(lib): add barrel export for lib utilities

---
+++ src/lib/index.ts
@@ -0,0 +1,13 @@
+export { cn } from './cn';
+export { formatStx, formatCompact, formatCurrency, formatPercent } from './format-number';
+export { formatRelative, formatTimestamp } from './format-date';
+export { copyToClipboard } from './clipboard';
+export { getStorageItem, setStorageItem, removeStorageItem } from './storage';
+export { sleep } from './sleep';
+export { invariant } from './invariant';
+export { debounce } from './debounce';
+export { throttle } from './throttle';
+export { createStrictContext } from './create-context';
+export { env } from './env';
+
+export type {};
