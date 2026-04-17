4aa12fb24e0af7e7caf83afa6474790967ab917e
winsznx <timjosh507@gmail.com>
2026-04-17 21:32:32 +0100

    chore(hooks): add barrel export for custom hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,14 @@
+export { useDebounce } from './use-debounce';
+export { useMediaQuery } from './use-media-query';
+export { useClickOutside } from './use-click-outside';
+export { useLocalStorage } from './use-local-storage';
+export { useScrollLock } from './use-scroll-lock';
+export { useClipboard } from './use-clipboard';
+export { useToggle } from './use-toggle';
+export { usePrevious } from './use-previous';
+export { useMounted } from './use-mounted';
+export { useKeyboardShortcut } from './use-keyboard-shortcut';
+export { useInterval } from './use-interval';
+export { useDocumentTitle } from './use-document-title';
+
+export type {};
