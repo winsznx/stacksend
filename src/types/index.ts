559699d0bec536b39c6a3f53eb934df01ad27eca
winsznx <timjosh507@gmail.com>
2026-04-17 21:33:01 +0100

    chore(types): add barrel export for shared type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type { WalletState, WalletUser } from './wallet';
+export type { Recipient, TransferRequest, TransferResult } from './transfer';
+export type { NetworkConfig, NetworkName } from './network';
+export type { PlanConfig, PlanTier } from './plan';
+
+export type {};
