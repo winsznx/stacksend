130daa8713b418bfb5b80c61e7ea635d083825c6
winsznx <timjosh507@gmail.com>
2026-04-22 20:42:48 +0100

    refactor: remove any type from global error handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -196,7 +196,7 @@ app.use((req, res) => {
 });
 
 // Error handler
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.error('❌ Unhandled error:', err);
     res.status(500).json({ error: 'Internal server error' });
 });
