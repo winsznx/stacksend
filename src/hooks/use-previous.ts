a799566499bf26a2725a8083b76ad66bcb1b7484
winsznx <timjosh507@gmail.com>
2026-04-23 22:43:52 +0100

    refactor(hooks): add optional initial value generic to usePrevious

--- src/hooks/use-previous.ts
+++ src/hooks/use-previous.ts
@@ -1,8 +1,12 @@
 import { useRef, useEffect } from 'react';
 
-export function usePrevious<T>(value: T): T | undefined {
-  const ref = useRef<T | undefined>(undefined);
-  useEffect(() => { ref.current = value; }, [value]);
+export function usePrevious<T>(value: T, initialValue?: T): T | undefined {
+  const ref = useRef<T | undefined>(initialValue);
+  
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  
   return ref.current;
 }
 
