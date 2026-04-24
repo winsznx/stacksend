7890cc659ebfdb7bfba784098d29a2308878cbf5
winsznx <timjosh507@gmail.com>
2026-04-24 17:13:02 +0100

    types: centralize exported constants types in index

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,4 +3,6 @@ export type { Recipient, TransferRequest, TransferResult } from './transfer';
 export type { NetworkConfig, NetworkName } from './network';
 export type { PlanConfig, PlanTier } from './plan';
 
+export type * as Constants from '../utils/constants';
+
 export type {};
