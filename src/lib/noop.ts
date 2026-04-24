4c8347f81d36d4a565b6c4b97125a9d843dec3e1
winsznx <timjosh507@gmail.com>
2026-04-24 17:13:01 +0100

    refactor(lib): make noop function strongly typed with generic return

--- src/lib/noop.ts
+++ src/lib/noop.ts
@@ -1,4 +1,4 @@
-export function noop(): void {}
+export const noop = <T = void>(...args: unknown[]): T => (undefined as unknown as T);
 
 export const EMPTY_ARRAY: readonly [] = Object.freeze([]) as readonly [];
 export const EMPTY_OBJECT: Readonly<Record<string, never>> = Object.freeze({});
